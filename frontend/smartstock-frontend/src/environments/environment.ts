06aa2b7c131caf896aff5db4ad83a19ae0ebfbb8
Sannidhya0208 <sannidhya966@gmail.com>
2026-08-21 15:44:29 +0530

    environment changes

--- frontend/smartstock-frontend/src/environments/environment.ts
+++ frontend/smartstock-frontend/src/environments/environment.ts
@@ -1,9 +1,9 @@
-// export const environment = {
-//   production: false,
-//   apiUrl: 'https://smartstock-ai-3.onrender.com/api'
-// };
-
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'https://smartstock-ai-3.onrender.com/api'
 };
+
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:8080/api'
+// };
